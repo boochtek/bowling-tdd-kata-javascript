0084ac5e1d530211f76bd575555e7b27a7948fed
Craig Buchek <craig@boochtek.com>
2021-08-18 11:27:47 -0500

    Results of my first practice run
    Note that the code isn't really what's important here.
    What's important for this kata is the TDD process.

--- bowling.js
+++ bowling.js
@@ -1,18 +1,71 @@
-//
-// This is only a SKELETON file for the 'Bowling' kata.
-// It's provided as a convenience to get you started writing code faster.
-//
-
 export class Game {
-  roll() {
-    throw new Error('Remove this statement and implement this function');
+  constructor() {
+    this.frames = [];
   }
+  roll = (pinsKnockedDown) => {
+    // NOTE: This can be combined with the next `if`.
+    if ( this.frames.length == 0 ) {
+      const frame = new Frame(pinsKnockedDown);
+      this.frames.push(frame);
+      return;
+    }
 
+    const lastFrame = this.frames[this.frames.length - 1];
+    const secondLastFrame = this.frames[this.frames.length - 2];
+
+    if ( lastFrame.isComplete() ) {
+      const frame = new Frame(pinsKnockedDown);
+      this.frames.push(frame);
+    } else {
+      lastFrame.roll(pinsKnockedDown);
+    }
+
+    if ( lastFrame && !lastFrame.isScoreComplete() ) {
+      lastFrame.roll(pinsKnockedDown);
+    }
+
+    if ( secondLastFrame && !secondLastFrame.isScoreComplete() ) {
+      secondLastFrame.roll(pinsKnockedDown);
+    }
+  }
   get score() {
-    throw new Error('Remove this statement and implement this getter');
+    return this.frames.reduce((acc, frames) => {
+      return acc + frames.score;
+    }, 0);
   }
 }
 
 export class Frame {
-
+  constructor(...rolls) {
+    this.rolls = rolls
+  }
+  get score() {
+    if ( this.isStrike() ) {
+      return this.rolls[0] + (this.rolls?.[1] || 0) + (this.rolls?.[2] || 0)
+    } else if ( this.isSpare() ) {
+      return this.rolls[0] + this.rolls[1] + (this.rolls?.[2] || 0)
+    } else {
+      return this.rolls[0] + (this.rolls?.[1] || 0)
+    }
+  }
+  isStrike = () => {
+    return this.rolls[0] == 10;
+  }
+  isSpare = () => {
+    return !this.isStrike() && (this.rolls[0] + (this.rolls?.[1] || 0) == 10)
+  }
+  isComplete = () => {
+    return (this.isStrike() && this.rolls.length == 1) ||
+           (this.isSpare() && this.rolls.length == 2) ||
+            this.rolls.length == 2;
+  }
+  isScoreComplete = () => {
+    if ( this.isStrike() || this.isSpare() ) {
+      return this.rolls.length == 3;
+    }
+    return this.rolls.length == 2;
+  }
+  roll = (pinsKnockedDown) => {
+    this.rolls.push(pinsKnockedDown);
+  }
 }
--- bowling.spec.js
+++ bowling.spec.js
@@ -7,15 +7,26 @@ import { Game, Frame } from './bowling';
 
 
 describe('Game', () => {
-  xdescribe('a game with no rolls', () => {
-    let game = new Game;
+  describe('a game with no rolls', () => {
+    let game = new Game();
     it ('scores 0 points', () => {
       expect(game.score).toEqual(0);
     })
   })
 
-  xdescribe('a game with no pins knocked down', () => {
-    let game = new Game;
+  describe('a game starting with 3 strikes', () => {
+    let game = new Game();
+    const rolls = [10, 10, 10];
+    rolls.forEach((roll) => {
+      game.roll(roll);
+    });
+    it ('scores 60 points', () => {
+      expect(game.score).toEqual(60);
+    })
+  })
+
+  describe('a game with no pins knocked down', () => {
+    let game = new Game();
     const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     rolls.forEach((roll) => {
       game.roll(roll);
@@ -25,8 +36,8 @@ describe('Game', () => {
     })
   })
 
-  xdescribe('a perfect game', () => {
-    let game = new Game;
+  describe('a perfect game', () => {
+    let game = new Game();
     const rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
     rolls.forEach((roll) => {
       game.roll(roll);
@@ -36,3 +47,112 @@ describe('Game', () => {
     })
   })
 })
+
+describe('Frame', () => {
+  describe('a frame with a strike', () => {
+    describe('and no following rolls', () => {
+      let frame = new Frame(10);
+      it ('is a strike', () => {
+        expect(frame.isStrike()).toBe(true);
+      })
+      it ('is not a spare', () => {
+        expect(frame.isSpare()).toBe(false);
+      })
+      it ('is a complete frame', () => {
+        expect(frame.isComplete()).toBe(true);
+      })
+      it ('scores 10 points', () => {
+        expect(frame.score).toEqual(10);
+      })
+      it ('has not been completely scored', () => {
+        expect(frame.isScoreComplete()).toBe(false);
+      })
+    })
+    describe('and 2 following rolls', () => {
+      let frame = new Frame(10, 2, 9);
+      it ('is a strike', () => {
+        expect(frame.isStrike()).toBe(true);
+      })
+      it ('is not a spare', () => {
+        expect(frame.isSpare()).toBe(false);
+      })
+      it ('scores 10 points, plus points for the following 2 rolls', () => {
+        expect(frame.score).toEqual(10 + 2 + 9);
+      })
+      it ('has been completely scored', () => {
+        expect(frame.isScoreComplete()).toBe(true);
+      })
+    })
+  })
+  describe('a frame with a spare', () => {
+    describe('and no following rolls', () => {
+      let frame = new Frame(5, 5);
+      it ('is a spare', () => {
+        expect(frame.isSpare()).toBe(true);
+      })
+      it ('is not a strike', () => {
+        expect(frame.isStrike()).toBe(false);
+      })
+      it ('is a complete frame', () => {
+        expect(frame.isComplete()).toBe(true);
+      })
+      it ('scores 10 points', () => {
+        expect(frame.score).toEqual(10);
+      })
+      it ('has not been completely scored', () => {
+        expect(frame.isScoreComplete()).toBe(false);
+      })
+    })
+    describe('and 1 following roll', () => {
+      let frame = new Frame(5, 5, 2);
+      it ('is a spare', () => {
+        expect(frame.isSpare()).toBe(true);
+      })
+      it ('is not a strike', () => {
+        expect(frame.isStrike()).toBe(false);
+      })
+      it ('scores 10 points, plus the number of pins knocked down in the following roll', () => {
+        expect(frame.score).toEqual(10 + 2);
+      })
+      it ('has been completely scored', () => {
+        expect(frame.isScoreComplete()).toBe(true);
+      })
+    })
+  })
+  describe('an open frame with 1 roll', () => {
+    let frame = new Frame(7);
+    it ('is not a strike', () => {
+      expect(frame.isStrike()).toBe(false);
+    })
+    it ('is not a spare', () => {
+      expect(frame.isSpare()).toBe(false);
+    })
+    it ('is not a complete frame', () => {
+      expect(frame.isComplete()).toBe(false);
+    })
+    it ('scores points for the number of pins knocked down in both rolls', () => {
+      expect(frame.score).toEqual(7);
+    })
+    it ('has not been completely scored', () => {
+      expect(frame.isScoreComplete()).toBe(false);
+    })
+  })
+  describe('an open frame with 2 rolls', () => {
+    let frame = new Frame(7, 2);
+    it ('is not a strike', () => {
+      expect(frame.isStrike()).toBe(false);
+    })
+    it ('is not a spare', () => {
+      expect(frame.isSpare()).toBe(false);
+    })
+    it ('is a complete frame', () => {
+      expect(frame.isComplete()).toBe(true);
+    })
+    it ('scores points for the number of pins knocked down in both rolls', () => {
+      expect(frame.score).toEqual(7 + 2);
+    })
+    it ('has been completely scored', () => {
+      expect(frame.isScoreComplete()).toBe(true);
+    })
+  });
+});
