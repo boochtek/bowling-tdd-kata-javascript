926b8bd5ffb8a8ff7e350e3f7a70ee782e51113d
Craig Buchek <craig@boochtek.com>
2021-08-18 12:50:32 -0500

    Add some top-level tests for some more game scores

--- bowling.spec.js
+++ bowling.spec.js
@@ -20,6 +20,28 @@ describe('Game', () => {
     })
   })
 
+  xdescribe('a game starting with 3 strikes', () => {
+    let game = new Game;
+    const rolls = [10, 10, 10];
+    rolls.forEach((roll) => {
+      game.roll(roll);
+    });
+    it ('scores 60 points', () => {
+      expect(game.score).toEqual(60);
+    })
+  })
+
+  xdescribe('a game starting with a mix of strikes, spares, and open frames', () => {
+    let game = new Game;
+    const rolls = [10, 2, 7, 5, 5, 6, 3];
+    rolls.forEach((roll) => {
+      game.roll(roll);
+    });
+    it ('be scored appropriately', () => {
+      expect(game.score).toEqual((10 + 2 + 7) + (2 + 7) + (10 + 6) + (6 + 3));
+    })
+  })
+
   xdescribe('a perfect game', () => {
     let game = new Game;
     const rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
