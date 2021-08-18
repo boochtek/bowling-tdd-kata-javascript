bb15f4b28d5656b36e3b846e9ef3def4124a8b02
Craig Buchek <craig@boochtek.com>
2021-08-18 11:31:35 -0500

    Create Bowling kata to use to practice TDD
    This is based on the Exercism JavaScript kata:
    https://github.com/exercism/javascript/tree/main/exercises/practice/bowling
    
    The original concept comes from this article on But Uncle Bob:
    http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata

---
+++ bowling.spec.js
@@ -0,0 +1,38 @@
+//
+// This is only a SKELETON file for the 'Bowling' kata tests.
+// It's provided as a convenience to get you started writing code faster.
+//
+
+import { Game } from './bowling';
+
+
+describe('Game', () => {
+  xdescribe('a game with no rolls', () => {
+    let game = new Game;
+    it ('scores 0 points', () => {
+      expect(game.score).toEqual(0);
+    })
+  })
+
+  xdescribe('a game with no pins knocked down', () => {
+    let game = new Game;
+    const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    rolls.forEach((roll) => {
+      game.roll(roll);
+    });
+    it ('scores 0 points', () => {
+      expect(game.score).toEqual(0);
+    })
+  })
+
+  xdescribe('a perfect game', () => {
+    let game = new Game;
+    const rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+    rolls.forEach((roll) => {
+      game.roll(roll);
+    });
+    it ('scores 300 points', () => {
+      expect(game.score).toEqual(300);
+    })
+  })
+})
