13be1eb19399c56cd4a7e1ede6c6f55c71163f0c
Craig Buchek <craig@boochtek.com>
2021-08-19 09:39:35 -0500

    Make spares and open frames scorable by Frame

--- bowling.js
+++ bowling.js
@@ -4,6 +4,23 @@ export class Game {
   }
 
   get score() {
-    return 0;
+    return this.frames.reduce((acc, frame) => {
+      return acc + frame.score;
+    }, 0);
+  }
+}
+
+
+export class Frame {
+  constructor(...rolls) {
+    this.rolls = rolls
+  }
+
+  roll(pins) {
+    this.rolls.push(pins)
+  }
+
+  get score() {
+    return this.rolls[0] + (this.rolls[1] || 0) + (this.rolls[2] || 0);
   }
 }
--- bowling.spec.js
+++ bowling.spec.js
@@ -1,4 +1,4 @@
-import { Game } from './bowling';
+import { Game, Frame } from './bowling';
 
 
 describe('Game', () => {
@@ -53,3 +53,64 @@ describe('Game', () => {
     })
   })
 })
+
+
+describe('Frame', () => {
+  describe('with a strike and no following rolls', () => {
+    let frame = new Frame(10);
+    it ('scores 10 points', () => {
+      expect(frame.score).toEqual(10);
+    })
+  })
+
+  describe('with a strike and 1 following roll', () => {
+    let frame = new Frame(10, 5);
+    it ('scores 10 points, plus the following roll', () => {
+      expect(frame.score).toEqual(10 + 5);
+    })
+  })
+
+  describe('with a strike and 2 following rolls', () => {
+    let frame = new Frame(10, 5, 4);
+    it ('scores 10 points, plus the pins knocked down in the 2 following rolls', () => {
+      expect(frame.score).toEqual(10 + 5 + 4);
+    })
+  })
+
+  describe('with a spare and no following rolls', () => {
+    let frame = new Frame(5, 5);
+    it ('scores 10 points', () => {
+      expect(frame.score).toEqual(10);
+    })
+  })
+
+  describe('with a spare and 1 following roll', () => {
+    let frame = new Frame(5, 5, 6);
+    it ('scores 10 points, plus the number of pins knocked down in the following roll', () => {
+      expect(frame.score).toEqual(10 + 6);
+    })
+  })
+
+  describe('with a spare and 2 following rolls', () => {
+    let frame = new Frame(5, 5, 6, 6);
+    it ('scores 10 points, plus the number of pins knocked down in the first following roll', () => {
+      expect(frame.score).toEqual(10 + 6);
+    })
+  })
+
+  describe('that is an open frame with no following rolls', () => {
+    let frame = new Frame(4, 5);
+    it ('scores the number of pins knocked down in the 2 rolls', () => {
+      expect(frame.score).toEqual(4 + 5);
+    })
+  })
+
+  describe('that is an open frame with 2 following rolls', () => {
+    let frame = new Frame(4, 5);
+    it ('scores the number of pins knocked down in the original 2 rolls', () => {
+      expect(frame.score).toEqual(4 + 5);
+    })
+  })
+
+})
+
